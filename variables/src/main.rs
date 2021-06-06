64edd466dd6d0759b960b55e1283d0aca65872ae
Felipe Marino <felipemarino91@gmail.com>
2021-06-06 16:32:25 +0200

    Add chapter 3 - variables

---
+++ variables/src/main.rs
@@ -0,0 +1,98 @@
+const MAX_POINTS: u32 = 100_000;
+
+fn main() {
+    const LOCAL_CONST: &str = "100_000";
+    println!("{}", MAX_POINTS.to_string() + LOCAL_CONST);
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    shadowing();
+}
+
+fn shadowing() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    let x = 3;
+    println!("The value of x is: {}", x);
+
+    // shadowing with different type
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    // of course mutating a type is not possible, given that Rust is a statically typed language
+    let mut spaces = "   ";
+    // spaces = spaces.len(); // error[E0308]: mismatched types
+}
+
+fn scalarTypes() {
+    // signed and unsigned integers
+    let x: i8 = 1; // -128 -> +127
+    let x: i16 = 1;
+    let x: i32 = 1;
+    let x: i64 = 1;
+    let x: i128 = 1;
+
+    let x: u8 = 1; // 0 -> 255
+    let x: u16 = 1;
+    let x: u32 = 1;
+    let x: u64 = 1;
+    let x: u128 = 1;
+
+    // float
+    let x = 2.0; // f64 - double precision - aka double
+    let y: f32 = 3.0; // f32 - single precision
+}
+
+fn numericOperations() {
+    // remainder
+    let remainder = 43 % 5;
+}
+
+fn tuples() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // pattern matching - destructuring the tuple
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    // accessing by the index also works - as in Swift
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+}
+
+fn arrays() {
+    let a = [1, 2, 3, 4, 5];
+
+    // different from vectors - that can grow in size
+    // arrays are generally used in cases where the data is static
+    // e.g. - months
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    // can also be declared with a shorthand that takes a value to repeat (3 in this case)
+    // and the size of the array (5)
+    let a = [3; 5]; // let a = [3, 3, 3, 3, 3]
+
+    // Nice things with e.g. accessing a invalid index of array is that
+    // Rust is more safe than other low level languages,
+    // since the invalid memory is not accessed, but instead the program
+    // immediately exits when it panics!
+}
